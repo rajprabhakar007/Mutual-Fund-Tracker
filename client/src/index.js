11a7051acb3ef034fd869423f9cef398f11b890e
Raj Prabhakar <rajprabhakar.git@gmail.com>
2025-06-25 01:10:07 +0530

    adding static.json file in public folder

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { BrowserRouter } from 'react-router-dom'; // ✅ Import BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -13,9 +14,9 @@ const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={clientId}>
-      <App />
+      <BrowserRouter> {/* ✅ Wrap App with BrowserRouter */}
+        <App />
+      </BrowserRouter>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
-
-
