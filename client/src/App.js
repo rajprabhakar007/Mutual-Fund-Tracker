6d03ac5e43084698dae080fc967e64da16730bb1
Raj Prabhakar <rajprabhakar.git@gmail.com>
2025-06-25 01:18:09 +0530

    adding static.json file in public folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -8,30 +8,25 @@ import FundDetails from './pages/FundDetails';
 import SavedFunds from './pages/SavedFunds';
 import Footer from './components/Footer';
 import GoogleSuccess from './pages/GoogleSuccess';
+
 function App() {
   return (
-    <Router>
-      <div className="d-flex flex-column min-vh-100">
-        
-        <Navbar />
+    <div className="d-flex flex-column min-vh-100">
+      <Navbar />
 
-        
-        <div className="container flex-grow-1 mt-4">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/fund/:code" element={<FundDetails />} />
-            <Route path="/saved" element={<SavedFunds />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/google-success" element={<GoogleSuccess />} />
-
-          </Routes>
-        </div>
-
-        
-        <Footer />
+      <div className="container flex-grow-1 mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/fund/:code" element={<FundDetails />} />
+          <Route path="/saved" element={<SavedFunds />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/google-success" element={<GoogleSuccess />} />
+        </Routes>
       </div>
-    </Router>
+
+      <Footer />
+    </div>
   );
 }
 
